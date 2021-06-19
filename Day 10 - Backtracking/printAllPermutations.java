898251dce44a0e35c16534b2db8806a75aec5134
Sarthak Saklecha <sarthaksaklecha1@gmail.com>
2021-06-20 01:34:57 +0530

    best

--- Day 10 - Backtracking/printAllPermutations.java
+++ Day 10 - Backtracking/printAllPermutations.java
@@ -37,3 +37,40 @@ class Solution {
         
     }
 }
+
+
+// Best
+// we don't need another ds to store the combination as well the isTaken array will also not be needed
+// we have a variable index which will keep track of the cuurent index, 
+// we will swap all the values in the array with index and call the function recursively on it
+// for e.g              *1* 23 
+//          1 *2* 3         2 *1* 3             3 *2* 1  (swap 0,0 swap 0,1, swap 0,2)
+// 1 2 *3*  1 3 *2*    2 1 *3*   2 3 *1*      3 2 *1*   3 1 *2*
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int index = 0;
+        helper(nums,index,result);
+        return result;
+    }
+    public void helper(int[] nums,int index,List<List<Integer>> result){
+        if(index==nums.length){
+            List<Integer> combination = new ArrayList<Integer>();
+            for(int i : nums) combination.add(i);
+            result.add(combination);
+            return;
+        };
+        for(int i=index;i<nums.length;i++){
+            // swap elements at index and i
+            swap(nums,index,i);
+            helper(nums,index+1,result);
+            swap(nums,index,i);
+        }
+    }
+    public void swap(int[] nums,int ind1,int ind2){
+        int temp = nums[ind1];
+        nums[ind1] = nums[ind2];
+        nums[ind2] = temp;
+        return;
+    }
+}
